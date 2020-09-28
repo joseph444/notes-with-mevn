2299d3604a0cfe8d4e8e6a87805f30f92846d6af
subhro <52913990+joseph444@users.noreply.github.com>
2020-09-29 03:31:35 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports={
     SALT: require("bcrypt").genSaltSync(),
     SECRET: require("crypto").randomBytes(64).toString("hex"),
-    DB:"mongodb+srv://subhro:OyRBwBwkqhozWXx8@cluster0.pfgey.gcp.mongodb.net/app_0?retryWrites=true&w=majority"
+    DB:"mongoDB url"
 }
